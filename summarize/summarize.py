48e43551eeba5cc2834dbca10987677f4d1d2f16
Milosz <milosh-dr@wp.pl>
2023-11-08 20:47:39 +0000

    Funcionality splitted into severak more logical units

--- summarize/summarize.py
+++ summarize/summarize.py
@@ -1,14 +1,15 @@
 # Update imports and venv
 
 # Update the code after experimenting in Colab
+import os
 
 from youtube_transcript_api import YouTubeTranscriptApi
 from openai import OpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
-# Figure out how to store api_key
-OPENAI_API_KEY = ''
+# Get the OpenAI api key from environment
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 
 def get_video_id(url):
@@ -47,11 +48,45 @@ def split_document(document):
     return docs
 
 
-def get_summary(docs):
-    client = OpenAI(api_key=OPENAI_API_KEY)
+def gpt_single_doc(doc):
 
-    if len(docs) == 1:
-        pass
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    
+    base_prompt = """Your job is to generate around 800 tokens long notes that a university student might create on a lecture.
+    The text you are going to use is a neuroscience podcast transcript. Make sure you include only points coming directly from the text.
+    Have the following principles in mind.
+    1. Exclude any information on podcast's sponsors and ways of supporting it.
+    2. Eliminate redunduncies if necessary
+    3. Edit the notes to a neatly formated, easy to follow bullet-style (possibly nested) structure
+    
+    Text:"""
+
+    try:
+        response = client.chat.completions.create(
+                        model="gpt-4-1106-preview",
+                        messages=[
+                            {
+                                'role': 'user',
+                                'content': '\n'.join([base_prompt, doc])
+                            }
+                        ],
+                        temperature=0.7,
+                        max_tokens=1000,
+                        top_p=1,
+                        frequency_penalty=0,
+                        presence_penalty=0
+                    )
+    except Exception as err:
+        print(err)
+        return None, err
+    
+    summary = response.choices[0].message.content
+    return summary, None
+
+
+def gpt_multiple_docs(docs):
+
+    client = OpenAI(api_key=OPENAI_API_KEY)
 
     responses = []
 
@@ -86,7 +121,8 @@ def get_summary(docs):
     
     text_for_gpt4 = text_for_gpt4.lstrip()
 
-    # Make sure the lenght is ok
+    # Make sure the lenght is ok, for now just print
+    print(f'The length of final text for gpt4: {len(text_for_gpt4)} characters (approx. {len(text_for_gpt4)//4} tokens)')
 
     gpt4_prompt = """
     Given the following notes made from several chunks of the same podcast transcript create the final version with the following principles in mind.
@@ -96,23 +132,37 @@ def get_summary(docs):
 
     Notes:
     """
-
-    final_response = client.chat.completions.create(
-                    model="gpt-4-1106-preview",
-                    messages=[
-                        {
-                            'role': 'user',
-                            'content': '\n'.join([gpt4_prompt, text_for_gpt4])
-                        }
-                    ],
-                    temperature=0.7,
-                    max_tokens=1000,
-                    top_p=1,
-                    frequency_penalty=0,
-                    presence_penalty=0
-                )
+    try:
+        final_response = client.chat.completions.create(
+                        model="gpt-4-1106-preview",
+                        messages=[
+                            {
+                                'role': 'user',
+                                'content': '\n'.join([gpt4_prompt, text_for_gpt4])
+                            }
+                        ],
+                        temperature=0.7,
+                        max_tokens=1000,
+                        top_p=1,
+                        frequency_penalty=0,
+                        presence_penalty=0
+                    )
+    except Exception as err:
+        print(err)
+        return None, err
+    
     summary = final_response.choices[0].message.content
-    return summary
+    return summary, None
+
+
+def get_summary(docs):
+
+    if len(docs) == 1:
+        summary = gpt_single_doc(docs[0])
+        return summary, None
+
+    summary = gpt_multiple_docs(docs)
+    return summary, None    
 
 
 def summarize(url):
@@ -131,3 +181,8 @@ def summarize(url):
     summary = get_summary(docs)
 
     return summary, None
+
+
+# For testing purposes
+if __name__ == '__main__':
+    summarize('')
